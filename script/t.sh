531209ce5db5b887d8b7112ae0d4d47a8143ebd8
Ihar Hancharenka <iigoncharenko@gmail.com>
2017-09-04 15:57:46 +0300

    publish

--- script/t.sh
+++ script/t.sh
@@ -2,6 +2,6 @@
 #export ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer
 #ASAN_OPTIONS=symbolize=1 build/exe/main/main
 
-valgrind --leak-check=yes build/exe/main/main
+#valgrind --leak-check=yes build/exe/main/main
 
-#build/exe/main/main
+build/exe/main/main
